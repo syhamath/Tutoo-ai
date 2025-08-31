8def5316bba138bfed0b37fe1f5a302f5f3b0674
syhamath <Hamath.Sy@apmterminals.com>
2025-08-31 09:58:31 +0000

    Update vite.config.ts
    Fix : motion/reat in all files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Clean config – no Tailwind plugin needed for v3
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "motion/react": "framer-motion"
+    }
+  }
 });
