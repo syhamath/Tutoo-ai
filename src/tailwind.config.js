8f5c97e48936814b7a6f927826a66f8c4dbd57c1
syhamath <Hamath.Sy@apmterminals.com>
2025-08-31 09:53:41 +0000

    Update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        "murshidi-sky-blue": "#1DA1F2",
+        "murshidi-purple": "#6C5CE7",
+        "murshidi-light-blue": "#E6F4FF",
+        "murshidi-light-purple": "#F1E8FF"
+      }
+    }
+  },
+  plugins: []
 };
